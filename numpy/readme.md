8e77fcb382deabc3d5b0847e7305fb6ccc89efbe
xiangzy <2695668485@qq.com>
2024-02-23 23:09:53 +0800

    2024/2/23晚上

---
+++ numpy/readme.md
@@ -0,0 +1,2 @@
+链接：[NumPy Ndarray 对象 | 菜鸟教程 (runoob.com)](https://www.runoob.com/numpy/numpy-ndarray-object.html)
+
--- python基本库/subprocess/Popen方法.py
+++ python基本库/subprocess/Popen方法.py
@@ -5,24 +5,17 @@ startupinfo=None, creationflags=0,restore_signals=True, start_new_session=False,
 *, encoding=None, errors=None)
 
 参数说明：
-args：shell命令，可以是字符串或者序列类型（如：list，元组）
-
-bufsize：缓冲区大小。当创建标准流的管道对象时使用，默认-1。
-0：不使用缓冲区
-1：表示行缓冲，仅当universal_newlines=True时可用，也就是文本模式
-正数：表示缓冲区大小
-负数：表示使用系统默认的缓冲区大小。
-
-stdin, stdout, stderr：分别表示程序的标准输入、输出、错误句柄
-
-preexec_fn：只在 Unix 平台下有效，用于指定一个可执行对象（callable object），它将在子进程运行之前被调用
-
-shell：如果该参数为 True，将通过操作系统的 shell 执行指定的命令。
-
-cwd：用于设置子进程的当前目录。
-
-env：用于指定子进程的环境变量。如果 env = None，子进程的环境变量将从父进程中继承。
-
+args： 要执行的shell命令，可以是字符串，也可以是命令各个参数组成的序列。当该参数的值是一个字符串时，该命令的解释过程是与平台相关的，因此通常建议将args参数作为一个序列传递。
+bufsize： 指定缓存策略，0表示不缓冲，1表示行缓冲，其他大于1的数字表示缓冲区大小，负数 表示使用系统默认缓冲策略。
+stdin, stdout, stderr： 分别表示程序标准输入、输出、错误句柄。
+preexec_fn： 用于指定一个将在子进程运行之前被调用的可执行对象，只在Unix平台下有效。
+close_fds： 如果该参数的值为True，则除了0,1和2之外的所有文件描述符都将会在子进程执行之前被关闭。
+shell： 该参数用于标识是否使用shell作为要执行的程序，如果shell值为True，则建议将args参数作为一个字符串传递而不要作为一个序列传递。
+cwd： 如果该参数值不是None，则该函数将会在执行这个子进程之前改变当前工作目录。
+env： 用于指定子进程的环境变量，如果env=None，那么子进程的环境变量将从父进程中继承。如果env!=None，它的值必须是一个映射对象。
+universal_newlines： 如果该参数值为True，则该文件对象的stdin，stdout和stderr将会作为文本流被打开，否则他们将会被作为二进制流被打开。
+startupinfo和creationflags： 这两个参数只在Windows下有效，它们将被传递给底层的CreateProcess()函数，用于设置子进程的一些属性，如主窗口的外观，进程优先级等。
+————————————————
 Popen 对象方法:
 poll(): 检查进程是否终止，如果终止返回 returncode，否则返回 None。
 wait(timeout): 等待子进程终止。
